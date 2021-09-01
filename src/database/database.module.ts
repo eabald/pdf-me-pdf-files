96a5fe96a4ddf88818e57b5da9f9693ba981692b
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-09-01 21:41:14 +0200

    added entity

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -6,6 +6,7 @@ import {
   TemplateEntity,
   UserEntity,
   LimitEntity,
+  PaymentEntity,
 } from '@pdf-me/shared';
 
 @Module({
@@ -20,7 +21,13 @@ import {
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
-        entities: [FileEntity, UserEntity, TemplateEntity, LimitEntity],
+        entities: [
+          UserEntity,
+          TemplateEntity,
+          FileEntity,
+          LimitEntity,
+          PaymentEntity,
+        ],
         synchronize: true,
         logging: true,
       }),
